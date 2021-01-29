aa4bd457a028aa9cfb851f8645554fa493e2582c
Dor Akerman <dor5894@gmail.com>
2021-01-29 03:34:54 +0200

    Bash works with files

--- firewall.sh
+++ firewall.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
+shopt -s xpg_echo
 
-MAX_RULES=6 	#Max number of rules in line is 4 we took extra care for special
-				#cases (line begins or ends with comma)
-results=""
-tot_packets=$(cat /dev/stdin)
+touch results.txt
+total_pkts=$(cat /dev/stdin)
 # For each line - remove spaces, extra commas and ignore comments
-while read line
+while read -r line
 do
-	curr_packets="$tot_packets"
-	echo "$tot_packets" > curr_packets.txt
 	line=$(echo "${line}" | sed 's/ //g' | awk -F# '{print $1}')
 	line=$(echo "${line}" | sed -E 's/,+/,/g')
-	for(( i=1; i<=$MAX_RULES; i++));do
-		temp_rule=$(echo "${line}" | awk -F, -v num="$i" '{print $num}')
-		if [[ "$temp_rule" != "" ]]; then
-			# curr_packets=$(echo "$curr_packets" | ./firewall.exe "$temp_rule")
-			
-			echo "$temp_rule"
-			./firewall.exe ""${temp_rule}"" < curr_packets.txt > curr_packets.txt
-			echo ------------------------------------------
-		fi
-	done
-	# echo "$curr_packets"
-	# echo --------------------------------
-	results+=$(echo $'\n')
-	results+=$(cat curr_packets.txt)
+	if [[ "${line::1}" == ',' ]]; then	#If line starts with commea
+		line=$(echo "$line" | sed 's/,//')
+	fi
+	if [[ "$line" != "" ]]; then
+		echo "$total_pkts" > curr_pkts.txt
+		IFS=',' read -r -a rule_array <<< "$line"
+		for rule in "${rule_array[@]}"
+		do
+			./firewall.exe "$rule" < curr_pkts.txt > temp_pkts.txt
+			cat temp_pkts.txt > curr_pkts.txt
+		done
+		cat curr_pkts.txt >> results.txt
+	fi
 done<$1		#Read rules file
-results=$(echo "$results" | sort -u | sed 's/ //g')
-echo "$results"
+cat results.txt | sort -u | sed 's/ //g'
+
+rm curr_pkts.txt results.txt temp_pkts.txt
